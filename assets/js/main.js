b85f53dda3aab7af8f17286b8efea582b0c82f30
NguyenLucSongNguyen <lucsongnguyen99@gmail.com>
2024-05-08 21:14:02 +0700

    update product and add contact.html

--- assets/js/main.js
+++ assets/js/main.js
@@ -3,13 +3,24 @@ const imgs=document.getElementsByTagName('img')
 setInterval(() => {
     
 }, 4000);
+
 const handleMinus= ()=>{
     const minus=document.getElementById("quantity")
-    minus.value=parseInt(minus.value)-1
-    minus.innerHTML=minus.value
+    if(parseInt(minus.value)==1){
+        minus.value=1
+    }
+    else{
+        minus.value=parseInt(minus.value)-1
+        minus.innerHTML=minus.value
+    }
 }
 const handlePlus=()=>{
     const minus=document.getElementById("quantity")
-    minus.value=parseInt(minus.value)+1
-    minus.innerHTML=minus.value
+    if(minus.value==10){
+        minus.value=10
+    }
+    else{
+        minus.value=parseInt(minus.value)+1
+        minus.innerHTML=minus.value
+    }
 }
